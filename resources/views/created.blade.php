55db40e05cd886e083bf9bf6bb1d9eecb795ab9c
maxim kinier <govormaxsim@gmail.com>
2022-01-23 14:22:45 +0300

    changed style for created.blade.php page

--- resources/views/created.blade.php
+++ resources/views/created.blade.php
@@ -2,6 +2,19 @@
 
 
 @section('content')
-    Старая ссылка {{$old}}
-    Новая сыслка {{url('/') . '/' . $new}}
+    <div class="created-links">
+        <div class="links-info">
+            <div class="link-info">
+                Сокращенная ссылка {{url('/') . '/' . $new}}
+            </div>
+            <hr>
+            <div class="link-info">
+                Будет перенаправлено на {{$old}}
+            </div>
+
+        </div>
+        <div class="home-link">
+            <a href="{{url('')}}">Домой</a>
+        </div>
+    </div>
 @endsection
