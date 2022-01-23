2f86f91fa5b33a78237313accb01866e2d50d129
maxim kinier <govormaxsim@gmail.com>
2022-01-23 15:25:40 +0300

    temporary button added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,13 @@
 @extends("layout.app")
 @section('content')
     <div class="wrapper">
-        <div class="linkForm">
+        <div class="site-name">
             Link shortifier
         </div>
+
+            <form action="{{url('/create')}}">
+                <button class="btn create-btn" type="submit">Click me</button>
+            </form>
+
     </div>
 @endsection
