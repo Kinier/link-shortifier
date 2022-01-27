c0c22e843d2dcc0417cb6d88bbd61715372cbc7c
maxim kinier <govormaxsim@gmail.com>
2022-01-27 15:35:19 +0300

    added check for errors variable

--- resources/views/created.blade.php
+++ resources/views/created.blade.php
@@ -4,14 +4,17 @@
 @section('content')
     <div class="created-links">
         <div class="links-info">
+            @if(!isset($errors))
             <div class="link-info">
-                Сокращенная ссылка {{url('/') . '/' . $new}}
+                Сокращенная ссылка {{url('/') . '/' . $new}} <!-- todo сделать проверка на эти переменные -->
             </div>
             <hr>
             <div class="link-info">
                 Будет перенаправлено на {{$old}}
             </div>
-
+            @else
+            <p>Как ты сюда попал? Возможно ты запретил куки? Если это так, тогда удачи с угадыванием ссылки</p>
+            @endif
         </div>
 
 
