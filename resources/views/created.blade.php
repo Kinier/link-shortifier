0f10b0cb01348578fe241197ea52f30ac508bb11
maxim kinier <govormaxsim@gmail.com>
2022-02-17 20:43:48 +0300

    notify for copy working now

--- resources/views/created.blade.php
+++ resources/views/created.blade.php
@@ -9,14 +9,14 @@
                 Сокращенная ссылка <p id="shorted-link">{{url('/') . '/' . $new}}</p> <!-- todo сделать проверка на эти переменные -->
             </div>
 
-                <button class="btn copy-btn" onclick="copyToClipboard('shorted-link'); return false;">Скопировать</button>
-
+                <button class="btn copy-btn" onclick="CopyToClipboard('shorted-link'); return false;">Скопировать</button>
+                <div hidden id="shorted-link-copied">Адрес скопирован</div>
             <hr>
             <div class="link-info">
                 Будет перенаправлено на <p id="link">{{$old}}</p>
             </div>
-                <button class="btn copy-btn" onclick="copyToClipboard('link'); return false;">Скопировать</button>
-
+                <button class="btn copy-btn" onclick="CopyToClipboard('link'); return false;">Скопировать</button>
+                <div id="link-copied" hidden>Адрес скопирован</div>
             @else
             <p>Ошибка. Если ты обновил страницу, то вернутся на главную можно нажав на надпись сверху.
                 Если ты ничего не нажимал то возможно у тебя отключены куки в браузере. Если это так, тогда включи
