4ef50e47741c943c9478cf805d56edb570d59e65
maxim kinier <govormaxsim@gmail.com>
2022-02-20 17:43:23 +0300

    todo added

--- app/Http/Controllers/apiControllers/apiLinkController.php
+++ app/Http/Controllers/apiControllers/apiLinkController.php
@@ -23,7 +23,7 @@ class apiLinkController extends Controller
         $url = $request->post('url');
         $data = ['url' => $url];
         if (Validator::make($data, ['url' => 'required|url'])->fails()){ // todo думаю перенести проверки в middleware
-            $data['error'] = 'seems this is not valid url';
+            $data['error'] = 'seems this is not valid url'; // todo проверку на существование сделать
             return json_encode($data);
         }
 
