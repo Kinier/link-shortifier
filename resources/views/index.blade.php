82d953b8bf782b51919e42b5d24edb412057024b
maxim kinier <govormaxsim@gmail.com>
2022-02-20 19:16:44 +0300

    documentation button add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
             <form class="shortify-form" action="{{url('/create')}}">
                 <button class="btn shortify-btn" type="submit">Сократить ссылку</button>
             </form>
-
+            <hr>
             <form class="shortify-form" action="{{url('/documentation/api/v1')}}">
                 <button class="btn" type="submit">Документация к API для сокращения ссылок</button>
             </form>
